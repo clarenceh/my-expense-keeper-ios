01a364277bd700dda8e9cca16a79b1f2d673f564
Clarence Ho <clarence@skywidesoft.com>
2013-05-06 09:26:53 +0800

    Initial Commit

---
+++ MyExpenseKeeper/MyExpenseKeeperAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MyExpenseKeeperAppDelegate.h
+//  MyExpenseKeeper
+//
+//  Created by Ho Clarence on 13年5月6日.
+//  Copyright (c) 2013年 Ho Clarence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyExpenseKeeperAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyExpenseKeeper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyExpenseKeeper
+//
+//  Created by Ho Clarence on 13年5月6日.
+//  Copyright (c) 2013年 Ho Clarence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MyExpenseKeeperAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MyExpenseKeeperAppDelegate class]));
+    }
+}
